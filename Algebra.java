8339a76a86d8341922e553996a455fd7835ff448
Noa Meghnagi <noameghnagi@Noas-MacBook-Air.local>
2025-11-17 22:17:45 +0200

    first try

--- Algebra.java
+++ Algebra.java
@@ -26,42 +26,83 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		for (int i =0; i<x2;i++){
+			result++;
+		}
+		return result;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		for (int i =0; i<x2;i++){
+			result--;
+		}
+		return result;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		for (int i =0; i<x2;i++){
+			result= plus(result, x1);
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x;
+		for (int i =0; i<n-1;i++){
+			result= times(result, x);
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		int count = 0;
+		while (result >= x2) {         
+  		int temp = x2;             
+    	while (temp != 0) {
+        result--;               
+        temp--;               
+    	}
+    	count++;                   
+	}
+		return count;
+
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		int count = 0;
+		while (result >= x2) {         
+  			int temp = x2;             
+    		while (temp != 0) {
+			result--;               
+			temp--;               
+    	}
+    	count++;                   
+		}
+		return result;
+
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		// Replace the following statement with your code
-		return 0;
+		int i =0;
+		int result=0;
+		while (times(i, i)<= x){
+			result = i;
+			i++;
+		}
+		return result;
 	}	  	  
 }
